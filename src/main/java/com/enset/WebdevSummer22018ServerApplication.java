30f14ecc9c6d8963f244d8f7239991bf5850c29e
funktion911 <kouider.e.s@gmail.com>
2021-05-23 23:12:00 +0100

    first commit

---
+++ src/main/java/com/enset/WebdevSummer22018ServerApplication.java
@@ -0,0 +1,13 @@
+package com.enset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevSummer22018ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevSummer22018ServerApplication.class, args);
+	}
+
+}
